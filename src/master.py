f9cd0ba93910fff5e28ee5e11874873073bc34dc
alexandre-minydetornaco <alexandre.miny@hotmail.com>
2019-09-19 18:09:53 +0200

    Merged dataset with pickel for matthieu

--- src/master.py
+++ src/master.py
@@ -2,26 +2,42 @@ import numpy as np
 import pandas as pd
 import os
 import sys
-import preprocessing
+import src.preprocessing
 
 
 def master_preprocessing_X(Path_AirportData, Path_WeatherData, Path_Aircraft, Path_correspondance_Aircraft_Airport):
 
  # Creation of airport and weather dataframe
-    X_airport = preprocessing.design_matrix_airport_data(Path_AirportData)
-    X_weather = preprocessing.weather_clean(Path_WeatherData)
+    X_airport = src.preprocessing.design_matrix_airport_data(Path_AirportData)
+    X_weather = src.preprocessing.weather_clean(Path_WeatherData)
 
 # Merging both dataframes
-    X_merged = preprocessing.join_weather_airport(X_airport, X_weather)
+    X_merged = src.preprocessing.join_weather_airport(X_airport, X_weather)
 
 # Creation of AC characteristics dataframe table of correspondances
-    df_charac = preprocessing.filtering_AC_charac(
+    df_charac = src.preprocessing.filtering_AC_charac(
         Path_correspondance_Aircraft_Airport, Path_Aircraft)
-    matching_dict = preprocessing.correspondance_dictionnary(
+    matching_dict = src.preprocessing.correspondance_dictionnary(
         Path_correspondance_Aircraft_Airport)
 
 # Merging the three dataset received
-    X_final = preprocessing.augmented_design_matrix_with_AC_charac(
+    X_final = src.preprocessing.augmented_design_matrix_with_AC_charac(
         X_merged, df_charac, matching_dict)
 
     return X_final
+
+
+
+
+########## Miny's Test ############
+#import os
+# import sys
+# import pandas as pd
+# sys.path.append("..")
+# from src import preprocessing
+# from src import master
+# master.master_preprocessing_X('../../0. Airport data/Airport_Data.csv',
+#                               '../../2. Weather data/weather_data_prep.csv',
+#                               '../../1. AC characteristics/ACchar.xlsx',
+#                               '../Correspondance.pkl')
+####################################
