c8b26051c9735a90fe9618429273be97b738f747
mdeborne <mathieu.deborne@mail.mcgill.ca>
2019-09-19 12:38:10 +0200

    update preprocessing .py

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,3 +1,4 @@
+## -------------------- ARNAUD ----------------------------------
 def glossary_feature_selection(path):
     ignore = ['To ignore', 'to ignore',  'Technical characteristic to ignore', 'Aircraft Type (with another regulation -not to be used for the case)']
     columns_df = pd.read_excel(path, sheet_name = 1).iloc[:,[0,2]]
