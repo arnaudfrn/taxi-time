03beabdd5f9fe99e321e6f87ac9e71b505dcacfc
alexandre-minydetornaco <alexandre.miny@hotmail.com>
2019-09-23 10:21:05 +0200

    Creation of base matrix

--- src/master.py
+++ src/master.py
@@ -2,29 +2,27 @@ import numpy as np
 import pandas as pd
 import os
 import sys
-from src.preprocessing import * 
+from src import preprocessing
 
 
 def master_preprocessing_X(Path_AirportData, Path_WeatherData, Path_Aircraft, Path_correspondance_Aircraft_Airport):
 
  # Creation of airport and weather dataframe
-    X_airport = design_matrix_airport_data(Path_AirportData)
+    X_airport = preprocessing.design_matrix_airport_data(Path_AirportData)
 
     ### X_airport = get_df_obs1(cleaning_airport_df(Path_AirportData)) (same with Tristan's functions)
 
-    X_weather = weather_clean(Path_WeatherData)
+    X_weather = preprocessing.weather_clean(Path_WeatherData)
 
 # Merging both dataframes
-    X_merged = join_weather_airport(X_airport, X_weather)
+    X_merged = preprocessing.join_weather_airport(X_airport, X_weather)
 
 # Creation of AC characteristics dataframe table of correspondances
-    df_charac = filtering_AC_charac(
-        Path_correspondance_Aircraft_Airport, Path_Aircraft)
-    matching_dict = correspondance_dictionnary(
-        Path_correspondance_Aircraft_Airport)
+    df_charac = preprocessing.filtering_AC_charac(Path_correspondance_Aircraft_Airport, Path_Aircraft)
+    matching_dict = preprocessing.correspondance_dictionnary(Path_correspondance_Aircraft_Airport)
 
 # Merging the three dataset received
-    X_final = augmented_design_matrix_with_AC_charac(X_merged, df_charac, matching_dict)
+    X_final = preprocessing.augmented_design_matrix_with_AC_charac(X_merged, df_charac, matching_dict)
 
 # Drop useless columns    
     X_final = X_final.drop(['Date Completed'], axis=1)
