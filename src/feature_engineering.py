2656ece0f32eeda91cbf12267a1cac77f383a065
alexandre-minydetornaco <alexandre.miny@hotmail.com>
2019-09-23 18:00:07 +0200

    modifiy metae function for feature engineering

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -38,11 +38,29 @@ def dummies(X,col_category) :
     X = pd.get_dummies(X, columns=col_category,drop_first=True)
     return X
 
-def drop_imput_scaled_dummies(X,col_numerical,col_to_drop,col_category):
+    ## df = final df from master.master_preprocessing
+    ## y = y from get_target.get_target
+    ## agg_fct is an aggregative fct eg:"mean"
+    ## drop is if we don't want the column anymore
+    ## column_list =['stand','AAC', 'ADG', 'TDG','Wake Category','ATCT Weight Class']
+def traget_encoding(df,column_list,y,agg_fct,drop=False):
+    for column in column_list:
+        target_enco_acType = pd.concat([df[column],y], axis=1)
+        target_enco_acType = target_enco_acType.groupby(df[column]).agg(agg_fct)
+        target_enco_acType = target_enco_acType.to_dict()['target']
+        df['target_encoding_'+column]=df[column].map(target_enco_acType)
+        if drop == True:
+            df.drop(columns=column, inplace=True)
+        else:
+            continue
+        return df
+
+#Function with mean for target encoding
+def drop_imput_encode_dummies(X,col_numerical,col_to_drop,col_category,col_to_target_encode,y,Path_airportData):
      X = drop_columns(X,col_to_drop)
      X = imputation(X,col_numerical)
-     #X = scaling(X,col_numerical)
      X = dummies(X,col_category)
+     X = traget_encoding(X,col_to_target_encode,y,"mean",drop=True)
      return X
 
 
@@ -66,21 +84,3 @@ def RMSE(y_test,pred):
     calculated_RMSE = np.sqrt(mean_squared_error(pred_in_min,y_test_in_min))
     return RMSE
 
-    ## df = final df from master.master_preprocessing
-    ## y = y from get_target.get_target
-    ## agg_fct is an aggregative fct eg:"mean"
-    ## drop is if we don't want the column anymore
-
-    ## column_list =['stand','AAC', 'ADG', 'TDG','Wake Category','ATCT Weight Class']
-
-def traget_encoding(df,column_list,y,agg_fct,drop=False):
-    for column in column_list:
-        target_enco_acType = pd.concat([df[column],y], axis=1)
-        target_enco_acType = target_enco_acType.groupby(df[column]).agg(agg_fct)
-        target_enco_acType = target_enco_acType.to_dict()['target']
-        df['target_encoding_'+column]=df[column].map(target_enco_acType)
-        if drop == True:
-            df.drop(columns=column, inplace=True)
-        else:
-            continue
-        return df
