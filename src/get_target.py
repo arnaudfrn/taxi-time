41520214bb04079bf8b8703109141481b7b42d47
alexandre-minydetornaco <alexandre.miny@hotmail.com>
2019-09-23 16:45:25 +0200

    creation of get_target function

---
+++ src/get_target.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+import os
+import sys
+from src import preprocessing
+from src import master
+
+def get_target(Path_airport):
+    y=preprocessing.get_target_values(
+        preprocessing.get_df_of_obs1(
+            preprocessing.cleaning_airport_df(Path_airport)
+            )
+                )
+    return y
